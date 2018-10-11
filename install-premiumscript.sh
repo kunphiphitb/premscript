70340d31c7a00bc39ae4ea982c5f13fb6c183d70
who am i <43548554+kunphiphitb@users.noreply.github.com>
2018-10-11 14:25:33 +0700

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Created by http://www.vps-murah.net
-# Modified by 0123456
+#
 
 cd
 sed -i '$ i\screen -AmdS limit /root/limit.sh' /etc/rc.local
@@ -21,7 +20,7 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/daybreakersx/premscript/master/premium-script.tar.gz"
+wget -O premium-script.tar.gz "https://raw.githubusercontent.com/kunphiphitb/premscript/master/premium-script.tar.gz"
 tar -xvf premium-script.tar.gz
 rm -f premium-script.tar.gz
 
@@ -79,5 +78,4 @@ echo " "
 echo "Premium Script Successfully Installed!"
 echo "Restarting all services..."
 echo "Wait for a few minutes..."
-echo "Modified by 0123456"
 echo " "
